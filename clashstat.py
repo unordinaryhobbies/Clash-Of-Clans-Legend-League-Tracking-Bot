aec9cebaa24ca9723ddd1b0985f793f19ec54bbc
dae-cmyk <74119677+dae-cmyk@users.noreply.github.com>
2021-06-20 20:29:44 +0900

    v 0.0.1

---
+++ clashstat.py
@@ -0,0 +1,114 @@
+import coc
+import nest_asyncio
+import asyncio
+import os
+
+class PlayerStats():
+    def __init__(self, ID: str, Password: str) -> None:
+        nest_asyncio.apply()
+        self.__ID: str = ID
+        self.__PW: str = Password
+        self.PlayersTag: list = []
+        self.client: coc.client.Client = coc.login(self.__ID, self.__PW)
+
+    def __del__(self) -> None:
+        try:
+            self.client.close()
+            print("Client Closed")
+        except Exception:
+            print("Client close failed")
+        
+    async def GetUserTrophies(self) -> list:
+        PlayersCoroutine: list = []
+        for player in self.PlayersTag:
+            PlayersCoroutine.append(asyncio.create_task(self.client.get_player(player)))
+            
+        PlayersDataPack: list = []
+        for Coroutine in PlayersCoroutine:
+            PlayersDataPack.append(await Coroutine)
+        
+        PlayersData: list = []
+        for PlayerData in PlayersDataPack:
+            PlayersData.append({'name': PlayerData.name, 'tag': PlayerData.tag, 'trophies': PlayerData.trophies})
+        return PlayersData
+
+    def GetPlayerList(self, Filename: str) -> None:
+        with open(Filename, 'r') as f:
+            while True:
+                tag = f.readline()
+                tag = tag.replace('\n', '')
+                if tag == '':
+                    break
+                self.PlayersTag.append(tag)
+    
+    def MakeLegendDatabase(self, PlayersInfo: list) -> None:
+        for PlayerInfo in PlayersInfo:
+            if os.path.isfile('{}.txt'.format(PlayerInfo.get('tag'))) is False:
+                with open('{}.txt'.format(PlayerInfo.get('tag')), 'w') as f:
+                    pass
+
+    def GetPlayerLatestInfoThroughTagFile(self) -> list:
+        PlayersDatabase = []
+        for Tag in self.PlayersTag:
+            LastestInfo = ''
+            with open('{}.txt'.format(Tag), 'r') as r:
+                while True:
+                    PlayerData = r.readline().replace('\n', '')
+                    if PlayerData == '':
+                        PlayersDatabase.append({'tag': Tag, 'trophies': LastestInfo})
+                        break
+                    LastestInfo = PlayerData
+        return PlayersDatabase
+    
+    def UpdateDatabase(self, PlayerInfo: dict) -> None:
+        if str(PlayerInfo.get('tag')):
+            if os.path.isfile('{}.txt'.format(PlayerInfo.get('tag'))) is False:
+                self.MakeLegendDatabase(PlayerInfo)
+            with open('{}.txt'.format(PlayerInfo.get('tag')), 'a') as a:
+                a.write('{}\n'.format(PlayerInfo.get('trophies')))
+
+    def CheckTrophyDifference(self, CurrentPlayerData, PastPlayerDataInDB) -> bool:
+        if CurrentPlayerData.get('tag') == PastPlayerDataInDB.get('tag'):
+            if PastPlayerDataInDB.get('trophies') == '':
+                return True
+            if int(PastPlayerDataInDB.get('trophies')) != int(CurrentPlayerData.get('trophies')):
+                return True
+            return False
+        else:
+            raise Exception("Tags unequal in CheckTrophyDiffernce()")
+
+    def FindTrophyDifference(self, CurrentPlayerData: int, PastPlayerData: int) -> int:
+        if CurrentPlayerData.get('tag') == PastPlayerData.get('tag'):
+            try:
+                print(".{}.".format(CurrentPlayerData.get('trophies')))
+                print(".{}.".format(PastPlayerData.get('trophies')))
+                TrophyDifference: int = int(CurrentPlayerData.get('trophies') - int(PastPlayerData.get('trophies')))
+                return TrophyDifference
+            except Exception:
+                return 0
+        else:
+            raise Exception("Tag not found in FindTrophyDifference()")
+
+    def Run(self) -> None:
+        PlayerUpdates: list = []
+
+        self.GetPlayerList('player.txt')
+
+        CurrentPlayersInfo = asyncio.run(self.GetUserTrophies())
+
+        self.MakeLegendDatabase(CurrentPlayersInfo)
+
+        PastPlayersData = self.GetPlayerLatestInfoThroughTagFile()
+
+        for CurrentPlayer, PastPlayer in zip(CurrentPlayersInfo, PastPlayersData):
+            if self.CheckTrophyDifference(CurrentPlayer, PastPlayer):
+                self.UpdateDatabase(CurrentPlayer)
+                TrophyChange = self.FindTrophyDifference(CurrentPlayer, PastPlayer)
+                if TrophyChange == 0:
+                    continue
+                if CurrentPlayer in PlayerUpdates:
+                    continue
+                PlayerUpdates.append({'tag': CurrentPlayer.get('tag'), 'trophies': TrophyChange, 'name': CurrentPlayer.get('name')})
+        
+        print(PlayerUpdates)
+        return PlayerUpdates
---
+++ main.py
@@ -0,0 +1,60 @@
+import discord
+from discord.ext import commands, tasks
+from clashstat import PlayerStats
+import os
+import asyncio
+import logging, traceback
+
+
+Client = commands.Bot(command_prefix='@')
+
+os.environ['ID'] = 'bigmart000918@gmail.com'
+os.environ['PW'] = 'dhrans99'
+os.environ['Channel'] = '856111241083617311'
+os.environ['Token'] = 'ODM2MzEyMjMyMTQwNDcyMzYw.YIcKdg.U03WSUurQjt9q_qebLnsAL9dgLA'
+
+
+ID = os.environ.get('ID')
+PW = os.environ.get('PW')
+ChannelID = os.environ.get('Channel')
+Token = os.environ.get('Token')
+
+coc = PlayerStats(ID, PW)
+
+async def MakeEmbed(Name: str, TrophyChange: str) -> discord.Embed:
+    if int(TrophyChange) > 0:
+        TrophyInStr = "+" + TrophyChange
+        Color = 0x00aaaa
+    else:
+        TrophyInStr = TrophyChange
+        Color = 0xFF8CFF
+    
+    embed = discord.Embed(title="{}  :  {}".format(Name, TrophyInStr), color=Color)
+    return embed
+
+@tasks.loop(minutes=2)
+async def Spy():
+    print("Running...")
+    Channel = Client.get_channel(int(ChannelID))
+
+    PlayersUpdate = coc.Run()
+
+    if len(PlayersUpdate) == 0:
+        return
+    print("Size of Update = {}".format(len(PlayersUpdate)))
+    for PlayerInfo in PlayersUpdate:
+        embed = await MakeEmbed(PlayerInfo.get('name'), str(PlayerInfo.get('trophies')))
+        await Channel.send(embed=embed)
+
+@Client.event
+async def on_ready():
+    print("On_ready")
+    print("Bot Login as {}".format(Client))
+
+    Spy.start()
+
+async def on_error(event, *args, **kwargs):
+    print('Something went wrong!')
+    logging.warning(traceback.format_exc())
+
+Client.run(Token)
